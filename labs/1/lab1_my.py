9c88f7c5d7761070b514247b4f916c4ec4b156f6
DrMatters <qseminq@gmail.com>
2020-11-27 03:43:14 +0300

    Debug some stuff

--- labs/1/lab1_my.py
+++ labs/1/lab1_my.py
@@ -10,16 +10,17 @@ from tqdm import tqdm
 ENABLE_SLEEP = True
 ENABLE_PRINTING = False
 RANDOM_SEED = 228
+TB_DIR = './tensorboard/'
 
 # constant parameters
-TOTAL_EPISODES = 70_000
+TOTAL_EPISODES = 20000
 MAX_STEPS = 100
 EPSILON_DECAY_RATE = 1 / TOTAL_EPISODES
 MIN_EPSILON = 0.001
 
 # rl params
-GAMMA = 0.7
-LR_RATE = 0.19
+GAMMA = 0.9
+LR_RATE = 0.5
 INIT_EPS = 1
 
 random.seed(RANDOM_SEED)
@@ -50,6 +51,7 @@ def ravel_env_state(env_state, observation_space):
 
 
 def main():
+    # writer = tensorboardX.SummaryWriter(TB_DIR, 'lab1_blackjack')
     # loading Frozen Lake environment from gym
     env = gym.make('Blackjack-v0')
     env.seed(RANDOM_SEED)
@@ -67,7 +69,7 @@ def main():
     Q_table = np.zeros((observation_space_size, env.action_space.n))
 
     # Start
-    for _ in tqdm(range(TOTAL_EPISODES)):
+    for episode in tqdm(range(TOTAL_EPISODES)):
         env_state = env.reset()
         state = ravel_env_state(env_state, env.observation_space)
         step = 0
@@ -83,6 +85,7 @@ def main():
             action = epsilon_greedy(state, epsilon, Q_table, env)
             new_state, reward, done, info = env.step(action)
             new_state = ravel_env_state(new_state, env.observation_space)
+            # writer.add_scalar('reward', reward, episode)
 
             # debug to see what's happening ("Ctrl + /" - uncomment highlighted code)
 
@@ -90,12 +93,12 @@ def main():
             # print("  {}       {}         {}".format(state, action, new_state))
             # env.render()
 
-            if done and (reward == 0):
-                reward = -10  # fell into the hole
-            elif done and (reward == 1):
-                reward = 100  # goal achieved
-            else:
-                reward = -1  # step on ice
+            # if done and (reward == 0):
+            #     reward = -10  # fell into the hole
+            # elif done and (reward == 1):
+            #     reward = 100  # goal achieved
+            # else:
+            #     reward = -1  # step on ice
 
             # doing the learning
             update_q_table(state, new_state, reward, action, Q_table)
@@ -125,15 +128,16 @@ def main():
 
     win = 0
     defeat = 0
+    draw = 0
 
     # Evaluation
-    for _ in tqdm(range(1000)):
+    for _ in tqdm(range(10000)):
         env_state = env.reset()
-        state = ravel_env_state(env_state, env.observation_space)
+        new_state = ravel_env_state(env_state, env.observation_space)
         step = 0
 
         while step < MAX_STEPS:
-            action = np.argmax(Q_table[state, :])
+            action = np.argmax(Q_table[new_state, :])
             new_state, reward, done, info = env.step(action)
             new_state = ravel_env_state(new_state, env.observation_space)
 
@@ -161,8 +165,10 @@ def main():
             if done:
                 if reward == 1:
                     win += 1
-                else:
+                elif reward == -1:
                     defeat += 1
+                else:
+                    draw += 1
                 break
 
             step += 1
@@ -173,7 +179,9 @@ def main():
                 time.sleep(1)
 
     win_rate = win / (win + defeat) * 100
+    # writer.close()
     print("Win rate: {}%".format(win_rate))
+    print(f'Wins: {win}\nDefeats: {defeat}\nDraw: {draw}')
 
 
 if __name__ == '__main__':
